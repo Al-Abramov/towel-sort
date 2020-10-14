3fa6309b941c338c1e6aa34ac31e8465ecae6667
Al-Abramov <x3m.f@mail.ru>
2020-10-14 10:14:54 +0400

    feat: add implement function for task Towel Sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 
 // You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+    if(!matrix){
+        return [];
+    }
+return matrix.map((arr, i) => (i % 2 === 1) ? arr.reverse() : arr).flat();
 }
+
